305a21e5e258f1173eed3b325ed9646075aa056e
ShilinRoman <shilinrom@gmail.com>
2024-05-26 13:56:49 +0300

    Update converterJSON_test.cpp
    Add test getResponsesLimit, getRequests, getFiles

--- tests/converterJSON_test.cpp
+++ tests/converterJSON_test.cpp
@@ -1,18 +1,11 @@
 #include "converterJSON.h"
+#include "invertedIndex.h"
 #include "gtest/gtest.h"
 
-class ConfigTest: public ::testing::Test
-{
-public:
-    ConverterJSON converter;
-};
-
-TEST_F(ConfigTest, readConfig)
-{
-    ASSERT_ANY_THROW(converter.getTextDocument());
-}
-
 TEST(converterJSON, readConfig) {
+    ConverterJSON converter;
+    json dict;
+
 
 }
 
@@ -21,17 +14,36 @@ TEST(converterJSON, readRequests) {
 }
 
 TEST(converterJSON, getResponsesLimit) {
-
+    ConverterJSON converter;
+    int expected = 5;
+    int responses = converter.getResponsesLimit();
+    ASSERT_EQ(expected, responses);
 }
 
 TEST(converterJSON, getRequests) {
-
+    ConverterJSON converter;
+    size_t expectedRequests = 1;
+    size_t numberRequests = converter.getRequests().size();
+    ASSERT_EQ(expectedRequests, numberRequests);
 }
 
 TEST(converterJSON, getFiles) {
+    std::ifstream file ("json_files\\config.json");
+    json dict;
+    file >> dict;
 
+    if (!dict["files"].empty())
+    {
+        auto expectedFiles = 26;
+        auto numberFiles = dict["files"].size();
+        ASSERT_EQ(expectedFiles, numberFiles);
+    }
+    else
+    {
+        std::cerr << "Error: no files" << std::endl;
+    }
 }
 
 TEST(converterJSON, putAnswers) {
-
+    
 }
